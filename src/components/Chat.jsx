3839d0566145aacc290ceeca000ef8e7cec032cb
Sakil Khan <sakilk130@gmail.com>
2021-10-26 15:02:39 +0600

    complete

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -55,7 +55,12 @@ function Chat() {
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3>{roomName}</h3>
-          <p>Last seen at ...</p>
+          <p>
+            Last seen at{' '}
+            {new Date(
+              messages[messages.length - 1]?.timestamp?.toDate()
+            ).toUTCString()}
+          </p>
         </div>
         <div className="chat__headerRight">
           <IconButton>
