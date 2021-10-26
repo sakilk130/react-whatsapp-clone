4833394501b03572dddda35816af2411e029b25d
Sakil Khan <sakilk130@gmail.com>
2021-10-26 12:57:21 +0600

    create login component and add google auth

---
+++ src/components/Login.jsx
@@ -0,0 +1,35 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import { auth, provider } from '../firebase/config';
+
+import '../styles/Login.css';
+
+function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+  return (
+    <div className="login">
+      <div className="login_container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+          alt=""
+        />
+        <div className="login_text">
+          <h1>Sign in to Whatsapp</h1>
+        </div>
+        <Button type="submit" onClick={signIn}>
+          Sign in With Google
+        </Button>
+      </div>
+    </div>
+  );
+}
+export default Login;
